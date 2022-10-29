f5c9feeebe17e43eb870b2d582580a96839438a8
ember arlynx <ember@lunar.town>
2022-10-28 22:13:06 -0400

    tmpl

---
+++ tests/e2e_tests.rs
@@ -0,0 +1,8 @@
+use pretty_assertions::assert_eq;
+
+/// TODO: Example end to end test, feel free to replace it with something meaningful.
+#[test]
+fn end_to_end() {
+    let x = 43;
+    assert_eq!(x, 43);
+}
---
+++ tests/integration_tests.rs
@@ -0,0 +1,15 @@
+use bantamlisp::CONFIG;
+use pretty_assertions::assert_eq;
+
+/// TODO: Example integration test, feel free to replace it with something meaningful.
+#[test]
+fn integrate() {
+    let x = 42;
+    assert_eq!(x, 42);
+}
+
+/// Make sure all necessary values are available in the environment.
+#[test]
+fn test_config() {
+    let _ = CONFIG.example_bool;
+}
